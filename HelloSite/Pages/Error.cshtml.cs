fd4a2306f2297d08d15d5b1056ec45b3ee9d196b
Your Name <1021776019@qq.com>
2018-06-05 20:15:12 +0800

    use latest sdk

--- HelloSite/Pages/Error.cshtml.cs
+++ HelloSite/Pages/Error.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace HelloSite.Pages
@@ -13,6 +14,7 @@ namespace HelloSite.Pages
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
